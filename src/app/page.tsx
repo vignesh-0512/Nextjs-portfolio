5e2da44d0a4977efb529e9fcb0611b5cd99759ac
Vignesh T <115525353+vignesh-0512@users.noreply.github.com>
2024-08-13 18:25:39 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
-import Navbar from '../components/Navbar'; // Adjust path if needed
-import Profile from '../components/Profile'; // Ensure correct path and capitalization
-import About from '../components/About'; // Ensure correct path and capitalization
-import Services from '../components/Services'; // Ensure correct path and capitalization
-import MyProjects from '../components/projects'; // Ensure correct path and capitalization
+import Navbar from '../components/Navbar';
+import Profile from '../components/Profile';
+import About from '../components/About';
+import Services from '../components/Services';
+import MyProjects from '../components/projects';
 import Testimonials from '../components/Testimonials';
-import Subscribe from '../components/subscribe';  
+import Subscribe from '../components/subscribe';
 import Footer from '../components/footer';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Corrected path
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar /> {/* Navbar component should display now */}
+      <Navbar />
       <Profile />
       <About />
       <Services />
       <MyProjects />
-      <Testimonials />  
+      <Testimonials />
       <Subscribe />
       <Footer />
-      {/* <h1>Welcome to My Portfolio</h1>
-      <p>This is the main page of my portfolio.</p> */}
-    </div>  
+    </div>
   );
 };
 
